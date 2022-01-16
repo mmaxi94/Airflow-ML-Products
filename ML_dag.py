b861aca360ca3a458ab3a74dd00c1fd785e8397a
Maximiliano Leonel Morales <62450950+mmaxi94@users.noreply.github.com>
2022-01-16 20:00:47 -0300

    Add files via upload

--- ML_dag.py
+++ ML_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 from dags.programs.ML_etl import get_products
+from airflow.operators.bash import BashOperator
 
 
 with DAG('ML_dag', start_date=datetime(2022, 1, 1), schedule_interval='@daily', catchup=False) as dag:
@@ -9,7 +10,13 @@ with DAG('ML_dag', start_date=datetime(2022, 1, 1), schedule_interval='@daily', 
         task_id="task_a",
         python_callable=get_products,
         op_kwargs={
-            "producto": "{{ var.value.ML_dag_producto }}"
-            
-        }
+            "producto": "{{ var.value.ML_dag_producto }}"        
+        }    
     )
+
+    task_b = BashOperator(
+            task_id="task_b",
+            bash_command="cd /opt/airflow/plugins/ ; gzip Productos.txt"
+    )
+
+    task_a >> task_b
